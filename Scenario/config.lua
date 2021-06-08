f1be0a0a2a98afac95b9ccf405c75510bac8a74d
Russel Delainey <russel.delainey@gmail.com>
2021-06-08 14:32:19 -0600

    Looks like it wraps text on it's own as required.

--- Scenario/config.lua
+++ Scenario/config.lua
@@ -1,16 +1,13 @@
 return {
     SCENARIO_TEXT =
-    "ATR is a cooperative game that is intended to provide some continuity \n" ..
-    "across map resets. \n" ..
-    "Rules: Be polite. Ask before changing other player's stuff. Have fun! \n" ..
-    "",
+    "ATR is a cooperative game that is intended to provide some continuity across map resets. \n" ..
+    "Rules: Be polite. Ask before changing other player's stuff. Have fun!",
     CONTACT_TEXT = "SteamID:Starholme | Russel.Delainey@gmail.com | Discord:Starholme#3744",
     SOFTMOD_TEXT = "Soft Mods:\n"..
     "Global Chat - Chat text is shared across all servers in the cluster",
     MOD_TEXT = "Mods:\n"..
-    "Clusterio Library - Used to communicate with the cluster"..
-    "Subspace Storage - Allows storing items and fluids in a limitless space, " ..
-    "   shared across the cluster",
+    "Clusterio Library - Used to communicate with the cluster\n"..
+    "Subspace Storage - Allows storing items and fluids in a limitless space, shared across the cluster",
 
     ENABLE_RESEARCH_QUEUE = true,
     FRIENDLY_FIRE = false
