c7e61e52acc3c12c8fb06a5950c08dd7f57e45c1
Russel Delainey <russel.delainey@gmail.com>
2021-08-06 15:52:30 -0600

    Fix transposed factorio ports, add public ip allow

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -28,9 +28,9 @@ sudo ufw default deny outgoing
 sudo ufw allow OpenSSH
 sudo ufw allow http
 sudo ufw allow https
-sudo ufw allow from 172.16.1.0/24 to any port 8181
-sudo ufw allow from 172.16.1.0/24 to any port 9090
-sudo ufw allow 31490:31500/udp
+sudo ufw allow from 216.197.161.145 to any port 8181
+sudo ufw allow from 216.197.161.145 to any port 9090
+sudo ufw allow 34190:34200/udp
 sudo ufw enable
 
 echo Completed. Please run upandauto to update and restart the system.
