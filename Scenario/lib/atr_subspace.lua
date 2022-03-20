97b5e13d9411ad6575a120cf0d4ee76f1f1a08e7
Russel Delainey <russel.delainey@gmail.com>
2022-03-20 17:41:28 -0600

    Clear entities first, store last spawned

--- Scenario/lib/atr_subspace.lua
+++ Scenario/lib/atr_subspace.lua
@@ -4,6 +4,11 @@ local CHANCE_OF_SUBSPACE_PAD = 100 --Chance is 1/this constant, so 1 over 100, o
 --REQUIRES--
 local CONFIG = require("config")
 
+--GLOBAL
+--global.atr_subspace{
+--    last_spawned = "someentity"
+--}
+
 --Holds items that are exported
 local exports = {}
 
@@ -15,28 +20,40 @@ local function spawn_subspace_pad(chunk_position)
     local tiles = {}
     local tile
 
+    --Clear any junk
+    for _, v in pairs(surface.find_entities({top_left, {top_left.x + 32,top_left.y+32}})) do
+        if v.name ~= "character" then
+            v.destroy()
+        end
+    end
+
     -- Create a concrete square
     for x=top_left.x,top_left.x + 32 do
         for y=top_left.y,top_left.y + 32 do
             tile = {name="refined-concrete", position={x,y}}
+            if x == top_left.x or x == top_left.x + 32
+            or y == top_left.y or y == top_left.y + 32 then
+                tile.name = "refined-hazard-concrete-left"
+            end
             table.insert(tiles,tile)
         end
     end
     surface.set_tiles(tiles)
 
     --Add the entity
-    local rand = math.random(4)
+    local subspace = global.atr_subspace or {}
+    local last_spawned = subspace.last_spawned or "subspace-item-injector"
     local name
-    game.print("rand:"..rand)
-    if rand == 1 then
+    if last_spawned == "subspace-fluid-extractor" then
         name = "subspace-item-injector"
-    elseif rand == 2 then
+    elseif last_spawned == "subspace-item-injector" then
         name = "subspace-item-extractor"
-    elseif rand == 3 then
+    elseif last_spawned == "subspace-item-extractor" then
         name = "subspace-fluid-injector"
-    else
+    elseif last_spawned == "subspace-fluid-injector" then
         name = "subspace-fluid-extractor"
     end
+    subspace.last_spawned = name
     local entity = surface.create_entity({name=name, position = {top_left.x + 16, top_left.y + 16}, force="player"})
     entity.destructible = false
     entity.minable = false
