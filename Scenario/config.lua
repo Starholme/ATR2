81093d6746fd8014c5892d61b47de83520ff66f9
Russel Delainey <russel.delainey@gmail.com>
2022-04-17 11:15:00 -0600

    Prep 1.5.1

--- Scenario/config.lua
+++ Scenario/config.lua
@@ -1,5 +1,5 @@
 return {
-    VERSION = "V1.5.0",
+    VERSION = "V1.5.1",
     SCENARIO_TEXT =
     "ATR is a cooperative game that is intended to provide some continuity across map resets. \n" ..
     "Rules: Be polite. Ask before changing other player's stuff. Have fun!",
@@ -11,7 +11,8 @@ return {
     SOFTMOD_TEXT = "Soft Mods:\n"..
     "Global Chat - Chat text is shared across all servers in the cluster\n"..
     "Server Select - Jump directly between servers in the cluster\n"..
-    "Vehicle Snap - Cars are easier to drive on roads, they snap to one of 8 directions",
+    "Vehicle Snap - Cars are easier to drive on roads, they snap to one of 8 directions\n"..
+    "Split Spawn - Have your own spawn point, invite others to it",
     DISCORD = "https://discord.gg/6dq2CbJ3Gx",
     CONTACT_TEXT = "See stats and server info at https://AllTheRockets.duckdns.org | Discord:Starholme#3744",
 
